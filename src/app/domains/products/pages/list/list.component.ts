b9f4b166cc357b8c18218386a1882e541956766b
Stiven Arboleda <stiven2201@gmail.com>
2024-11-09 00:27:14 -0500

    [ ADD ] Add product list and about pages. First version

---
+++ src/app/domains/products/pages/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './../../components/product/product.component'
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule, ProductComponent],
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+  fromChild(event: string) {
+    console.log('estamos en el padre');
+    console.log(event);
+  }
+}
